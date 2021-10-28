b109cfbd0c9c41aed11a7bd30a3222960f320fa8
Dimitar Angelov <dmapostbox@gmail.com>
2021-10-28 20:52:13 +0300

    reduced the number of parameters to test for RF

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ from sklearn import metrics
 from sklearn.metrics import confusion_matrix
 
 from data_preprocessing import train, y_train, test, y_test
-
+from getResults import getResults
 
 # Naïve Bayes
 NB_paramGrid = {'alpha': [0, 0.5, 1, 2]}
@@ -29,7 +29,7 @@ print(confusion_matrix(y_test, y_pred))
 
 # --------------------------
 # Logistic Regression
-LR_paramGrid = {'penalty': ['l1', 'l2'], 'C': [0.1, 0.5, 1, 5, 10]}
+LR_paramGrid = {'penalty': ['l1', 'l2'], 'C': [0.01, 0.05, 0.1, 0.25, 0.5, 1, 5, 10]}
 
 LR_gridSearch_model = GridSearchCV(LogisticRegression(solver='liblinear'), LR_paramGrid, cv=10, n_jobs=-1)
 LR_gridSearch_model.fit(train, y_train)
@@ -43,7 +43,6 @@ print(f"Test set score: {LR_gridSearch_model.score(test, y_test)}")
 
 print(metrics.classification_report(y_test, y_pred))
 print(confusion_matrix(y_test, y_pred))
-
 
 # --------------------------
 # Classification Trees
@@ -64,7 +63,7 @@ print(confusion_matrix(y_test, y_pred))
 
 # --------------------------
 # Random Forests
-RF_paramGrid = {'n_estimators': [10, 50, 100, 200, 500], 'max_depth': [None, 2, 4, 8, 16], 'min_samples_split': [2, 4, 8, 16], 'ccp_alpha': [0, 0.01, 0.02, 0.03, 0.05, 0.1]}
+RF_paramGrid = {'n_estimators': [10, 50, 100, 200, 500], 'max_depth': [None, 2, 4], 'min_samples_split': [2, 4], 'ccp_alpha': [0, 0.01, 0.02, 0.03, 0.05, 0.1]}
 
 RF_gridSearch_model = GridSearchCV(RandomForestClassifier(), RF_paramGrid, cv=10, n_jobs=-1)
 RF_gridSearch_model.fit(train, y_train)
@@ -78,3 +77,6 @@ print(f"Test set score: {RF_gridSearch_model.score(test, y_test)}")
 
 print(metrics.classification_report(y_test, y_pred))
 print(confusion_matrix(y_test, y_pred))
+
+# --------------------------
+getResults([NB_gridSearch_model, LR_gridSearch_model, DT_gridSearch_model, RF_gridSearch_model], False, True)
