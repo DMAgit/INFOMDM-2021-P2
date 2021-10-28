15533861dbd60e2e2c519f3d8ac14ece06401392
Dimitar Angelov <dmapostbox@gmail.com>
2021-10-28 20:51:01 +0300

    Created a config file

---
+++ config.py
@@ -0,0 +1,11 @@
+# Config for the data preprocessing
+
+# CountVectorizer
+ngram_range = (1, 1)  # The lower and upper boundary of the range of n-values for different word n-grams or char n-grams to be extracted.
+min_df = 1
+
+# Word normalization
+stemming = False
+lemmatization = False
+lowercase = False
+remove_stops = False
