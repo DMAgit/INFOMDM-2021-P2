1c15dcb0c8d8c58875b5ac3e8821748939384833
Dimitar Angelov <dmapostbox@gmail.com>
2021-11-01 22:54:03 +0200

    Updated range of parameters for multiple models

--- models.py
+++ models.py
@@ -13,7 +13,7 @@ from getResults import getResults
 
 # --------------------------
 # Naïve Bayes
-NB_paramGrid = {'alpha': [0.01, 0.1, 0.25, 0.5, 1, 2]}
+NB_paramGrid = {'alpha': [0.01, 0.1, 0.25, 0.5, 1, 2.5, 10]}
 
 NB_gridSearch_model = GridSearchCV(MultinomialNB(), NB_paramGrid, cv=10, n_jobs=-1)
 NB_gridSearch_model.fit(train, y_train)
@@ -30,7 +30,7 @@ print(confusion_matrix(y_test, y_pred))
 
 # --------------------------
 # Logistic Regression
-LR_paramGrid = {'penalty': ['l1', 'l2'], 'C': [0.01, 0.05, 0.1, 0.25, 0.5, 1, 5, 10]}
+LR_paramGrid = {'penalty': ['l1', 'l2'], 'C': [0.01, 0.1, 0.25, 0.5, 1, 2.5, 5, 10]}
 
 LR_gridSearch_model = GridSearchCV(LogisticRegression(solver='liblinear'), LR_paramGrid, cv=10, n_jobs=-1)
 LR_gridSearch_model.fit(train, y_train)
@@ -47,7 +47,8 @@ print(confusion_matrix(y_test, y_pred))
 
 # --------------------------
 # Classification Trees
-DT_paramGrid = {'max_depth': [None, 2, 4, 8, 16], 'min_samples_split': [2, 4, 8, 16], 'ccp_alpha': [0, 0.01, 0.02, 0.03, 0.05, 0.1]}
+DT_paramGrid = {'max_depth': [None, 2, 4, 8, 16], 'min_samples_split': [2, 4, 8, 16],
+                'ccp_alpha': [0, 0.01, 0.02, 0.03, 0.05, 0.1]}
 
 DT_gridSearch_model = GridSearchCV(DecisionTreeClassifier(), DT_paramGrid, cv=10, n_jobs=-1)
 DT_gridSearch_model.fit(train, y_train)
@@ -64,7 +65,8 @@ print(confusion_matrix(y_test, y_pred))
 
 # --------------------------
 # Random Forests
-RF_paramGrid = {'n_estimators': [10, 50, 100, 200, 500], 'max_depth': [None, 2, 4], 'min_samples_split': [2, 4], 'ccp_alpha': [0, 0.01, 0.02, 0.03, 0.05, 0.1]}
+RF_paramGrid = {'n_estimators': [10, 50, 100, 500, 1000, 5000], 'max_depth': [None, 2, 4],
+                'min_samples_split': [2, 4], 'ccp_alpha': [0, 0.01, 0.02, 0.03, 0.05, 0.1]}
 
 RF_gridSearch_model = GridSearchCV(RandomForestClassifier(), RF_paramGrid, cv=10, n_jobs=-1)
 RF_gridSearch_model.fit(train, y_train)
